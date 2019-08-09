Fix decoder's masking mechanism

train.py
@@ -16,6 +16,7 @@ from dataset import TranslationDataset, paired_collate_fn
 from transformer.Models import Transformer
 from transformer.Optim import ScheduledOptim
 
+
 def cal_performance(pred, gold, smoothing=False):
     ''' Apply label smoothing if needed '''
 
@@ -33,7 +34,7 @@ def cal_performance(pred, gold, smoothing=False):
 def cal_loss(pred, gold, smoothing):
     ''' Calculate cross entropy loss, apply label smoothing if needed. '''
 
-    gold = gold.contiguous().view(-1)
+    gold = gold.contiguous().view(-1)  # [batch size x num q tokens]
 
     if smoothing:
         eps = 0.1
@@ -64,10 +65,9 @@ def train_epoch(model, training_data, optimizer, device, smoothing):
     for batch in tqdm(
             training_data, mininterval=2,
             desc='  - (Training)   ', leave=False):
-
         # prepare data
         src_seq, src_pos, tgt_seq, tgt_pos = map(lambda x: x.to(device), batch)
-        gold = tgt_seq[:, 1:]
+        gold = tgt_seq[:, 1:]  # [num sentences, max tokens per sentence] # take all 'next' tgt tokens
 
         # forward
         optimizer.zero_grad()
@@ -88,9 +88,10 @@ def train_epoch(model, training_data, optimizer, device, smoothing):
         n_word_total += n_word
         n_word_correct += n_correct
 
-    loss_per_word = total_loss/n_word_total
-    accuracy = n_word_correct/n_word_total
+    loss_per_word = total_loss / n_word_total
+    accuracy = n_word_correct / n_word_total
     return loss_per_word, accuracy
+
 
 def eval_epoch(model, validation_data, device):
     ''' Epoch operation in evaluation phase '''
@@ -105,7 +106,6 @@ def eval_epoch(model, validation_data, device):
         for batch in tqdm(
                 validation_data, mininterval=2,
                 desc='  - (Validation) ', leave=False):
-
             # prepare data
             src_seq, src_pos, tgt_seq, tgt_pos = map(lambda x: x.to(device), batch)
             gold = tgt_seq[:, 1:]
@@ -122,9 +122,10 @@ def eval_epoch(model, validation_data, device):
             n_word_total += n_word
             n_word_correct += n_correct
 
-    loss_per_word = total_loss/n_word_total
-    accuracy = n_word_correct/n_word_total
+    loss_per_word = total_loss / n_word_total
+    accuracy = n_word_correct / n_word_total
     return loss_per_word, accuracy
+
 
 def train(model, training_data, validation_data, optimizer, device, opt):
     ''' Start training '''
@@ -150,17 +151,17 @@ def train(model, training_data, validation_data, optimizer, device, opt):
         start = time.time()
         train_loss, train_accu = train_epoch(
             model, training_data, optimizer, device, smoothing=opt.label_smoothing)
-        print('  - (Training)   ppl: {ppl: 8.5f}, accuracy: {accu:3.3f} %, '\
+        print('  - (Training)   ppl: {ppl: 8.5f}, accuracy: {accu:3.3f} %, ' \
               'elapse: {elapse:3.3f} min'.format(
-                  ppl=math.exp(min(train_loss, 100)), accu=100*train_accu,
-                  elapse=(time.time()-start)/60))
+            ppl=math.exp(min(train_loss, 100)), accu=100 * train_accu,
+            elapse=(time.time() - start) / 60))
 
         start = time.time()
         valid_loss, valid_accu = eval_epoch(model, validation_data, device)
-        print('  - (Validation) ppl: {ppl: 8.5f}, accuracy: {accu:3.3f} %, '\
-                'elapse: {elapse:3.3f} min'.format(
-                    ppl=math.exp(min(valid_loss, 100)), accu=100*valid_accu,
-                    elapse=(time.time()-start)/60))
+        print('  - (Validation) ppl: {ppl: 8.5f}, accuracy: {accu:3.3f} %, ' \
+              'elapse: {elapse:3.3f} min'.format(
+            ppl=math.exp(min(valid_loss, 100)), accu=100 * valid_accu,
+            elapse=(time.time() - start) / 60))
 
         valid_accus += [valid_accu]
 
@@ -172,7 +173,7 @@ def train(model, training_data, validation_data, optimizer, device, opt):
 
         if opt.save_model:
             if opt.save_mode == 'all':
-                model_name = opt.save_model + '_accu_{accu:3.3f}.chkpt'.format(accu=100*valid_accu)
+                model_name = opt.save_model + '_accu_{accu:3.3f}.chkpt'.format(accu=100 * valid_accu)
                 torch.save(checkpoint, model_name)
             elif opt.save_mode == 'best':
                 model_name = opt.save_model + '.chkpt'
@@ -184,10 +185,11 @@ def train(model, training_data, validation_data, optimizer, device, opt):
             with open(log_train_file, 'a') as log_tf, open(log_valid_file, 'a') as log_vf:
                 log_tf.write('{epoch},{loss: 8.5f},{ppl: 8.5f},{accu:3.3f}\n'.format(
                     epoch=epoch_i, loss=train_loss,
-                    ppl=math.exp(min(train_loss, 100)), accu=100*train_accu))
+                    ppl=math.exp(min(train_loss, 100)), accu=100 * train_accu))
                 log_vf.write('{epoch},{loss: 8.5f},{ppl: 8.5f},{accu:3.3f}\n'.format(
                     epoch=epoch_i, loss=valid_loss,
-                    ppl=math.exp(min(valid_loss, 100)), accu=100*valid_accu))
+                    ppl=math.exp(min(valid_loss, 100)), accu=100 * valid_accu))
+
 
 def main():
     ''' Main function '''
@@ -198,7 +200,7 @@ def main():
     parser.add_argument('-epoch', type=int, default=10)
     parser.add_argument('-batch_size', type=int, default=64)
 
-    #parser.add_argument('-d_word_vec', type=int, default=512)
+    # parser.add_argument('-d_word_vec', type=int, default=512)
     parser.add_argument('-d_model', type=int, default=512)
     parser.add_argument('-d_inner_hid', type=int, default=2048)
     parser.add_argument('-d_k', type=int, default=64)
@@ -223,7 +225,7 @@ def main():
     opt.cuda = not opt.no_cuda
     opt.d_word_vec = opt.d_model
 
-    #========= Loading Dataset =========#
+    # ========= Loading Dataset =========#
     data = torch.load(opt.data)
     opt.max_token_seq_len = data['settings'].max_token_seq_len
 
@@ -232,7 +234,7 @@ def main():
     opt.src_vocab_size = training_data.dataset.src_vocab_size
     opt.tgt_vocab_size = training_data.dataset.tgt_vocab_size
 
-    #========= Preparing Model =========#
+    # ========= Preparing Model =========#
     if opt.embs_share_weight:
         assert training_data.dataset.src_word2idx == training_data.dataset.tgt_word2idx, \
             'The src/tgt word2idx table are different but asked to share word embedding.'
@@ -261,7 +263,7 @@ def main():
             betas=(0.9, 0.98), eps=1e-09),
         opt.d_model, opt.n_warmup_steps)
 
-    train(transformer, training_data, validation_data, optimizer, device ,opt)
+    train(transformer, training_data, validation_data, optimizer, device, opt)
 
 
 def prepare_dataloaders(data, opt):

transformer/Layers.py
@@ -35,14 +35,23 @@ class DecoderLayer(nn.Module):
         self.pos_ffn = PositionwiseFeedForward(d_model, d_inner, dropout=dropout)
 
     def forward(self, dec_input, enc_output, non_pad_mask=None, slf_attn_mask=None, dec_enc_attn_mask=None):
-        dec_output, dec_slf_attn = self.slf_attn(
+        '''
+        :param dec_input: [batch size, num q tokens, d_model]
+        :param enc_output: [batch size, num k tokens, d_model]
+        :param non_pad_mask:  [batch size, num q tokens, 1] for masking out axis-1 rows corresponding to padding q tokens
+                                axis 0      axis 1       axis 2
+        :param slf_attn_mask: [batch size, num q tokens, num q tokens] for masking out padding q tokens and future q tokens in axis 2
+        :param dec_enc_attn_mask: [batch size, num q tokens, num k tokens] for masking out padding k tokens in axis 2
+        :return:
+        '''
+        dec_output, dec_slf_attn = self.slf_attn(  # each q token doesn't apply attention over future q tokens
             dec_input, dec_input, dec_input, mask=slf_attn_mask)
         dec_output *= non_pad_mask
 
-        dec_output, dec_enc_attn = self.enc_attn(
+        dec_output, dec_enc_attn = self.enc_attn(  # each q token apply attention over all k tokens of encoder
             dec_output, enc_output, enc_output, mask=dec_enc_attn_mask)
         dec_output *= non_pad_mask
-
+        # [batch size, num q tokens, d_model]
         dec_output = self.pos_ffn(dec_output)
         dec_output *= non_pad_mask
 

transformer/SubLayers.py
@@ -6,6 +6,7 @@ from transformer.Modules import ScaledDotProductAttention
 
 __author__ = "Yu-Hsiang Huang"
 
+
 class MultiHeadAttention(nn.Module):
     ''' Multi-Head Attention module '''
 
@@ -31,48 +32,49 @@ class MultiHeadAttention(nn.Module):
 
         self.dropout = nn.Dropout(dropout)
 
-
     def forward(self, q, k, v, mask=None):
-
         d_k, d_v, n_head = self.d_k, self.d_v, self.n_head
 
         sz_b, len_q, _ = q.size()
         sz_b, len_k, _ = k.size()
         sz_b, len_v, _ = v.size()
 
-        residual = q
+        residual = q  # [batch size, num q tokens, d_model]
 
         q = self.w_qs(q).view(sz_b, len_q, n_head, d_k)
         k = self.w_ks(k).view(sz_b, len_k, n_head, d_k)
         v = self.w_vs(v).view(sz_b, len_v, n_head, d_v)
 
-        q = q.permute(2, 0, 1, 3).contiguous().view(-1, len_q, d_k) # (n*b) x lq x dk
-        k = k.permute(2, 0, 1, 3).contiguous().view(-1, len_k, d_k) # (n*b) x lk x dk
-        v = v.permute(2, 0, 1, 3).contiguous().view(-1, len_v, d_v) # (n*b) x lv x dv
+        q = q.permute(2, 0, 1, 3).contiguous().view(-1, len_q,
+                                                    d_k)  # [n_heads x batch_size, num q_tokens, dk]
+        k = k.permute(2, 0, 1, 3).contiguous().view(-1, len_k, d_k)  # [n_heads x batch_size, num key tokens, dk]
+        v = v.permute(2, 0, 1, 3).contiguous().view(-1, len_v, d_v)  # [n_heads x batch_size, num key tokens, dv]
 
-        mask = mask.repeat(n_head, 1, 1) # (n*b) x .. x ..
-        output, attn = self.attention(q, k, v, mask=mask)
+        mask = mask.repeat(n_head, 1, 1)  # [n_heads x batch_size, num q_tokens, num key tokens]
+        output, attn = self.attention(q, k, v, mask=mask)  # [n_heads x batch size, num q tokens, dv]
 
         output = output.view(n_head, sz_b, len_q, d_v)
-        output = output.permute(1, 2, 0, 3).contiguous().view(sz_b, len_q, -1) # b x lq x (n*dv)
+        output = output.permute(1, 2, 0, 3).contiguous().view(sz_b, len_q,
+                                                              -1)  # [batch size, num q tokens, n_heads x dv]
 
-        output = self.dropout(self.fc(output))
+        output = self.dropout(self.fc(output))  # [batch size, num q tokens, d_model]
         output = self.layer_norm(output + residual)
 
         return output, attn
+
 
 class PositionwiseFeedForward(nn.Module):
     ''' A two-feed-forward-layer module '''
 
     def __init__(self, d_in, d_hid, dropout=0.1):
         super().__init__()
-        self.w_1 = nn.Conv1d(d_in, d_hid, 1) # position-wise
-        self.w_2 = nn.Conv1d(d_hid, d_in, 1) # position-wise
+        self.w_1 = nn.Conv1d(d_in, d_hid, 1)  # position-wise
+        self.w_2 = nn.Conv1d(d_hid, d_in, 1)  # position-wise
         self.layer_norm = nn.LayerNorm(d_in)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
-        residual = x
+        residual = x  # [batch size, num q tokens, d_model]
         output = x.transpose(1, 2)
         output = self.w_2(F.relu(self.w_1(output)))
         output = output.transpose(1, 2)

transformer/Translator.py
@@ -7,6 +7,7 @@ import torch.nn.functional as F
 from transformer.Models import Transformer
 from transformer.Beam import Beam
 
+
 class Translator(object):
     ''' Load with trained model and handle the beam search '''
 
@@ -132,24 +133,27 @@ class Translator(object):
             return all_hyp, all_scores
 
         with torch.no_grad():
-            #-- Encode
+            # -- Encode
             src_seq, src_pos = src_seq.to(self.device), src_pos.to(self.device)
             src_enc, *_ = self.model.encoder(src_seq, src_pos)
 
-            #-- Repeat data for beam search
-            n_bm = self.opt.beam_size
+            # -- Repeat data for beam search
+            n_bm = self.opt.beam_size  # = 4
             n_inst, len_s, d_h = src_enc.size()
-            src_seq = src_seq.repeat(1, n_bm).view(n_inst * n_bm, len_s)
+            # src_seq: [batch size, num k tokens]
+            src_seq = src_seq.repeat(1, n_bm).view(n_inst * n_bm,
+                                                   len_s)  # repeat(num repeat times of axis 0, num repeat times of axis 1, ...)
+            # src_enc: [batch size, num k tokens, d_model]
             src_enc = src_enc.repeat(1, n_bm, 1).view(n_inst * n_bm, len_s, d_h)
 
-            #-- Prepare beams
+            # -- Prepare beams
             inst_dec_beams = [Beam(n_bm, device=self.device) for _ in range(n_inst)]
 
-            #-- Bookkeeping for active or not
+            # -- Bookkeeping for active or not
             active_inst_idx_list = list(range(n_inst))
             inst_idx_to_position_map = get_inst_idx_to_tensor_position_map(active_inst_idx_list)
 
-            #-- Decode
+            # -- Decode
             for len_dec_seq in range(1, self.model_opt.max_token_seq_len + 1):
 
                 active_inst_idx_list = beam_decode_step(